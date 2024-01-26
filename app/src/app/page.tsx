0718855470c994558fd6757ae687ddf6339cfd89
participator <7463405+participator@users.noreply.github.com>
2024-01-26 01:40:27 -0800

    Setup Google Material Icons

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,7 +4,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <h1>Todos</h1>
+
+      <div>
+        <div><span className="material-symbols-outlined">check_box_outline_blank</span></div>
+      </div>
     </main>
   );
 }
