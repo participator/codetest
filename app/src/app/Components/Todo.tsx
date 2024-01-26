2ab91055a6798210f2183280644c597e5549f63d
participator <7463405+participator@users.noreply.github.com>
2024-01-26 13:04:25 -0800

    Update Todo Component to be noneditable

--- app/src/app/Components/Todo.tsx
+++ app/src/app/Components/Todo.tsx
@@ -1,22 +1,11 @@
-'use client'
 import styles from "./page.module.css"
-import { useState } from "react"
 
 export default function Todo(props) {
-    const { todo } = props
+    const { todo, displayMultiSelect } = props
     console.log(todo)
 
-    const [dateValue, setDateValue] = useState(new Date())
-
-    const createValidDate = (string) => {
-        const date = new Date(string)
-        const day = date.getDate() + 1
-        const dayFormatted =  day < 10 ? `0${day}` : day
-        const month = date.getMonth() + 1
-        const monthFormatted =  month < 10 ? `0${month}` : month
-        const year = date.getFullYear()
-
-        return `${year}-${monthFormatted}-${dayFormatted}`
+    const disableInputs = () => {
+        return false
     }
 
     return (
@@ -24,25 +13,33 @@ export default function Todo(props) {
             <span className="todo_status material-symbols-outlined">check</span>
             <div className="todo_details">
                 <p className="todo_details_title">Start Code Test</p>
-                <input 
-                    type="date"
-                    className="todos_details_date"
-                    value={createValidDate(dateValue)}
-                    onChange={({target}) => {
-                        setDateValue(new Date(target.value))
-                    }} />
+                <p className="todo_details_date">Jan 24, 2024</p>
             </div>
             <div className="todo_actions">
                 <div>
                     <span 
                         className="material-symbols-outlined"
-                        onClick={() => console.log('edit')}>
+                        onClick={() => console.log('displayEditForm')}>
                             edit
                     </span>
-                    <span className="material-symbols-outlined">delete</span>
-                    <span className="material-symbols-outlined">check_box_outline_blank</span>
+                    <span 
+                        className="material-symbols-outlined"
+                        onClick={() => console.log('send delete to api')}>
+                            delete
+                    </span>
+                    {
+                        displayMultiSelect &&
+                        <span
+                            className="material-symbols-outlined"
+                            onClick={() => console.log('display when multiselect is checked')}>
+                                check_box_outline_blank
+                        </span>
+                    }
                 </div>
-                <span className="todo_actions_more material-symbols-outlined">expand_more</span>
+                <span 
+                    className="todo_actions_more material-symbols-outlined">
+                        expand_more
+                </span>
             </div>
         </div>
     )
