58dd29eea3762a2f5a730c205cfa501161270792
participator <7463405+participator@users.noreply.github.com>
2024-01-26 02:00:21 -0800

    Mock Todo structure

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Todos</h1>
 
+      <div><span className="material-symbols-outlined">check_box_outline_blank</span></div>
+      <hr></hr>
       <div>
-        <div><span className="material-symbols-outlined">check_box_outline_blank</span></div>
+        <span className="material-symbols-outlined">check</span>
+        <div className="item">
+          Start Code Test
+          <p>Jan 24, 2024</p>
+        </div>
+        <div>
+          <div>
+            <span className="material-symbols-outlined">edit</span>
+            <span className="material-symbols-outlined">delete</span>
+            <span className="material-symbols-outlined">check_box_outline_blank</span>
+          </div>
+            <span className="material-symbols-outlined">expand_more</span>
+        </div>
       </div>
     </main>
-  );
+  )
 }
