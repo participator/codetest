0c36a39f4a26e68dfaeb78acabc6100086bb563a
participator <7463405+participator@users.noreply.github.com>
2024-01-26 02:13:48 -0800

    Split SelectAll into its own component

---
+++ app/src/app/Components/SelectAll.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css"
+
+export default function SelectAll(props) {
+    console.log('selectAll', props)
+
+    return (
+      <div><span className="material-symbols-outlined">check_box_outline_blank</span></div>
+    )
+}
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import styles from "./page.module.css"
 import Todo from "./Components/Todo"
+import SelectAll from "./Components/SelectAll"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Todos</h1>
 
-      <div><span className="material-symbols-outlined">check_box_outline_blank</span></div>
+      <SelectAll />
       <hr></hr>
       <Todo />
     </main>
