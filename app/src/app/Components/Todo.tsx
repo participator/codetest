e0d681de197264d933162746df25eb8e7cd08f30
participator <7463405+participator@users.noreply.github.com>
2024-01-26 16:08:25 -0800

    Add styles to todo
    - Improve status, title, and date styles

--- app/src/app/Components/Todo.tsx
+++ app/src/app/Components/Todo.tsx
@@ -1,21 +1,30 @@
-import styles from "./page.module.css"
+import styles from "./todo.module.css"
+
+const {
+    todo,
+    todo_status,
+    todo_details,
+    todo_details_title,
+    todo_details_date,
+    todo_actions
+} = styles
 
 export default function Todo(props) {
-    const { todo, displayMultiSelect } = props
-    console.log(todo)
+    const { displayMultiSelect } = props
+
 
     const disableInputs = () => {
         return false
     }
 
     return (
-        <div className="todo">
-            <span className="todo_status material-symbols-outlined">check</span>
-            <div className="todo_details">
-                <p className="todo_details_title">Start Code Test</p>
-                <p className="todo_details_date">Jan 24, 2024</p>
+        <div className={todo}>
+            <span className={`material-symbols-outlined ${todo_status}`}>{ true ? 'check' : 'close' }</span>
+            <div className={todo_details}>
+                <p className={todo_details_title}>Start Code Test</p>
+                <p className={todo_details_date}>Jan 24, 2024</p>
             </div>
-            <div className="todo_actions">
+            <div className={todo_actions}>
                 <div>
                     <span 
                         className="material-symbols-outlined"
