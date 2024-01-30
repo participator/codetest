64ffcc9d794522c0aaa8693c8af92a61f51a064f
participator <7463405+participator@users.noreply.github.com>
2024-01-29 22:55:29 -0800

    Get todo from database

--- app/src/app/api/todo/route.tsx
+++ app/src/app/api/todo/route.tsx
@@ -1,31 +1,9 @@
+import prisma from "../../../../prisma/lib/prisma"
 
 // GET /api/todo
-export function GET() {
+export async function GET() {
   // Get todos from db
-  const todos = [{
-    id: 0,
-    date: 'Jan 24, 2024',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    deleted: false,
-    done: true,
-    title: "Start Code Test",
-  },
-  {
-    id: 1,
-    date: 'Jan 28, 2024',
-    description: "Some more Ipsum",
-    deleted: false,
-    done: false,
-    title: "Complete Code Test",
-  },
-  {
-    id: 2,
-    date: 'Jan 28, 2024',
-    description: "Some mmore more Ipsum",
-    deleted: false,
-    done: false,
-    title: "Complete Code Test Again",
-  }]
+  const todos = await prisma.todo.findMany()
 
   return Response.json(todos)
 }
