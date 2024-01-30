181a95e8f9ce075ef620d2de2f2f8606ee0fecbe
participator <7463405+participator@users.noreply.github.com>
2024-01-29 22:54:06 -0800

    Add periods to sentences in seed data

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -4,30 +4,30 @@ const prisma = new PrismaClient()
 
 const todoData: Prisma.TodoCreateInput[] = [
   {
-    title: 'Discover Full Stack Engineer position with Roaming Hunger via job email list',
-    description: `Read about job description
-                    Get excited about Roaming Hunger in the job description
-                    Read more about Roaming Hunger on company's website`,
+    title: 'Discover Full Stack Engineer position with Roaming Hunger',
+    description: `Read about job description.
+                    Get excited about Roaming Hunger in the job description.
+                    Read more about Roaming Hunger on company's website.`,
     dateCreated: new Date('2024-01-18')
   },
   {
     title: 'Complete the application',
-    description: 'Create a thoughtful cover letter',
+    description: 'Create a thoughtful cover letter.',
     dateCreated: new Date('2024-01-18')
   },
   {
     title: 'Get contacted by the People & Culture Coordinator',
-    description: `Become really excited
-                    Schedule a chat with the People & Culture Coordinator to learn about the Roaming Hunger and the role`,
+    description: `Become really excited.
+                    Schedule a chat with the People & Culture Coordinator to learn about the Roaming Hunger and the role.`,
     dateCreated: new Date('2024-01-19')
   },
   {
     title: 'Chat with the People & Culture Coordinator',
-    description: `Learn about the company
-                    Learn that culture is higher priority than technological knowledge
-                    Learn more details about RMNG
-                    Share my experience and personality
-                    Become estatic about the opportunity`,
+    description: `Learn about the company.
+                    Learn that culture is higher priority than technological knowledge.
+                    Learn more details about RMNG.
+                    Share my experience and personality.
+                    Become estatic about the opportunity.`,
     dateCreated: new Date('2024-01-22')
   },
   {
@@ -37,7 +37,7 @@ const todoData: Prisma.TodoCreateInput[] = [
   },
   {
     title: 'Start code test',
-    description: 'Create Design (https://codetestdesign.framer.website/), Create Data model (https://docs.google.com/drawings/d/1UTzKAbcyrbcMjdFivp7HZZyDQtH8KJqhrUsU35pSmus), Start repo',
+    description: 'Create Design (https://codetestdesign.framer.website/). Create Data model (https://docs.google.com/drawings/d/1UTzKAbcyrbcMjdFivp7HZZyDQtH8KJqhrUsU35pSmus). Start repo.',
     dateCreated: new Date('2024-01-24')
   },
   {
@@ -47,7 +47,7 @@ const todoData: Prisma.TodoCreateInput[] = [
   },
   {
     title: 'Deploy code test',
-    description: 'Try to deply to cloud provider like Vercel or Google Cloud\'s Cloud Run',
+    description: 'Deply to cloud provider like Vercel or Google Cloud\'s Cloud Run.',
     dateCreated: new Date('2024-01-29')
   },
   {
