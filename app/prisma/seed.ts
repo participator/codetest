d6a50b0e133afb57ff4220013b0d57086dfb29a0
participator <7463405+participator@users.noreply.github.com>
2024-02-01 00:16:47 -0800

    Update seed file
    - Add new lines in long description
    - Add todos with max character lengths

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -5,9 +5,7 @@ const prisma = new PrismaClient()
 const todoData: Prisma.TodoCreateInput[] = [
   {
     title: 'Discover Full Stack Engineer position with Roaming Hunger',
-    description: `Read about job description.
-                    Get excited about Roaming Hunger in the job description.
-                    Read more about Roaming Hunger on company's website.`,
+    description: `Read about job description.\nGet excited about Roaming Hunger in the job description.\nRead more about Roaming Hunger on company's website.`,
     dateCreated: new Date('2024-01-18')
   },
   {
@@ -17,17 +15,12 @@ const todoData: Prisma.TodoCreateInput[] = [
   },
   {
     title: 'Get contacted by the People & Culture Coordinator',
-    description: `Become really excited.
-                    Schedule a chat with the People & Culture Coordinator to learn about the Roaming Hunger and the role.`,
+    description: `Become really excited.\nSchedule a chat with the People & Culture Coordinator to learn about the Roaming Hunger and the role.`,
     dateCreated: new Date('2024-01-19')
   },
   {
     title: 'Chat with the People & Culture Coordinator',
-    description: `Learn about the company.
-                    Learn that culture is higher priority than technological knowledge.
-                    Learn more details about RMNG.
-                    Share my experience and personality.
-                    Become estatic about the opportunity.`,
+    description: `Learn about the company.\nLearn that culture is higher priority than technological knowledge.\nLearn more details about RMNG.\nShare my experience and personality.\nBecome estatic about the opportunity.`,
     dateCreated: new Date('2024-01-22')
   },
   {
@@ -55,6 +48,16 @@ const todoData: Prisma.TodoCreateInput[] = [
     description: 'First day working as a Full Stack Developer with Roaming Hunger!',
     dateCreated: new Date('2024-02-19')
   },
+  {
+    title: 'Max description length',
+    description: 'hMGX8DLOMQ73L6nLmY8Z4KA9f3wXmEnbqsrh4I8J3n1JD1CUdEPU3fJAf1y0mjLY7DkZbIOJbSZmGDuurRG2B32J5guGD1CTY1xH14L4EP7CqlkmeW0tyPIjRUUmOJwlDDhI3j22EceV7lI64xzCtY9uLtjD7ZC80RIVsiJOwBe02uoefyB4tHFNIIRy5I3tz25CEAEoeddBwaY3Q7ulFXTwUCLbO6s9yLPH7RKUG3i6jcHN2UcOi0Q978TqDxPfAjfNTdQhlZofp2uyHWdYxVfSBkg89vin8TVk5WCRu2ijqpd16nCrviciykPAuZkW9o7x3wtnRsaZ9HOjb0a8sEY35AUpqy0BJ79swqjpbpGdE7WjFC8sbk1TfIR50IlCU0J1GovEwqN3O2IFPY07NGzkJfk40WHJ5i7GK1AUyZcupQBAl8XH0Tn3Q3llPGRxvQQAsewvPvxd4yIXXdtanYjcizSVI1i5RCja5RgGzE27AVEHk8z8',
+    dateCreated: new Date('2024-01-31')
+  },
+  {
+    title: 'f2F2vASfckGn38TOQ2LRS1gMcChVx4zbEDF7Eowa1bTWmxuxvB8HnXeQ88oT0Jc0ZFG1t8O1GTlDp0osQn5WCoDI5jbkiEoaJRYM',
+    description: 'Max title length',
+    dateCreated: new Date('2024-01-31')
+  }
 ]
 
 async function main() {
