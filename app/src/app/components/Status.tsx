0238f4af2dabb4a499b8e88f11a2e0e750d42f88
participator <7463405+participator@users.noreply.github.com>
2024-02-01 00:11:10 -0800

    Provide hover feedback for Status (Action) component

--- app/src/app/components/Status.tsx
+++ app/src/app/components/Status.tsx
@@ -1,21 +1,23 @@
 import styles from './status.module.css'
 
 const {
-    status
+    status,
+    status_checkbox,
+    status_icon
 } = styles
 
 export default function Status({ state, styles, handleStatusChange }) {
     const icon = state ? 'check' : 'close'
 
-    return <label className={`material-symbols-outlined ${styles}`}>
+    return <label className={`material-symbols-outlined ${status} ${styles}`}>
                 <input
-                    className={`${status} ${state ? 'status__done' : 'status__undone'}`}
+                    className={`${status_checkbox} ${state ? 'status__done' : 'status__undone'}`}
                     name="done"
                     type="checkbox"
                     checked={state}
                     onChange={handleStatusChange}
                 />
-                {icon}
+                <span className={status_icon}>{icon}</span>
             </label>
 
 }
